a775507c0d1d7c4e32f265c3edf24ec014f05ca7
Pedro Casagrande de Campos <pedro@mystra.com.br>
2017-03-02 18:20:56 -0300

    O bot aprende quem são as pessoas.

--- publish.js
+++ publish.js
@@ -2,9 +2,6 @@ const config = require('./config.json')
 var mqtt = require('mqtt')
 var client = mqtt.connect('mqtt://' + config.mqttserver)
 
-client.publish(config.mqtttopic, "oi1")
-client.publish(config.mqtttopic, "oi2")
-client.publish(config.mqtttopic, "oi3")
-client.publish(config.mqtttopic, "oi4")
+client.publish(config.mqtttopic, "Teste de comunicação.")
 
 client.end()
--- slack.js
+++ slack.js
@@ -2,6 +2,8 @@ const config = require('./config.json');
 const mqtt = require('mqtt');
 const client = mqtt.connect('mqtt://' + config.mqttserver);
 
+const empresas = {};
+
 var Bot = require('slackbots');
 var settings = {
     token: config.slackkey,
@@ -9,10 +11,19 @@ var settings = {
 };
 var bot = new Bot(settings);
 
+bot.on('message', function(data) {
+    if (data.type === 'message' && data.text.startsWith('<@U4A8AN27P>')) {
+      var found = data.text.match('<.*> O ([0-9]) é o (.*)')
+      empresas[found[1]] = found[2];
+    }
+});
+
 client.on('connect', function() {
   client.subscribe(config.mqtttopic);
 });
 
 client.on('message', function(topic, message) {
-  bot.postMessageToChannel('porteiro_iot', message.toString());
+  console.log(message.toString());
+  console.log(empresas[message.toString()] + ' vem abrir o portão.');
+  bot.postMessageToChannel('porteiro_iot', empresas[message.toString()] + ' vem abrir o portão.');
 });
