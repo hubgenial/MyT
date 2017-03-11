1879c83e97f492b1c0f957569f311d78ce16881f
marquesgabriel <gabrielf.pmarques@gmail.com>
2017-03-11 16:00:56 -0300

    tratamento da mesagem quando undefined

--- slack.js
+++ slack.js
@@ -13,7 +13,7 @@ var bot = new Bot(settings);
 
 bot.on('message', function(data) {
     if (data.type === 'message' && data.text.startsWith('<@U4A8AN27P>')) {
-      var found = data.text.match('<.*> O ([0-9]) é [oa] (.*)')
+      var found = data.text.match('<.*> O ([0-9]) é [oa] (.*)');
       empresas[found[1]] = found[2];
     }
 });
@@ -23,7 +23,15 @@ client.on('connect', function() {
 });
 
 client.on('message', function(topic, message) {
-  console.log(message.toString());
-  console.log(empresas[message.toString()] + ' vem abrir o portão.');
-  bot.postMessageToChannel('porteiro_iot', empresas[message.toString()] + ' vem abrir o portão.');
+  // console.log(topic);
+  // console.log(message.toString());
+  // verifica se a empresa esta cadastrada
+  if((empresas[message.toString()]) != undefined){
+    // console.log(empresas[message.toString()] + ' vem abrir o portão.');
+    // comentar abaixo para evitar o flood no canal durante o desenvolvimento
+    bot.postMessageToChannel('porteiro_iot', empresas[message.toString()] + ' vem abrir o portão.');
+  }else{
+    // TODO tratar de enviar um feedback a quem esta chamando avisando q o erro
+    console.log('Nao encontrado, tente outro');
+  }
 });
